1d14226565d507279df81ec7aae7c4cd4f6183ce
lucas-perata <contacto.perata@gmail.com>
2024-08-26 17:24:25 -0300

    ejercicios

--- TP1-Pr en consola/Program.cs
+++ TP1-Pr en consola/Program.cs
@@ -12,69 +12,76 @@ class CGF
         // Ejercicio6();
         // Ejercicio7();
         // Ejercicio8();
+        // Ejercicio9();
+        // Ejercicio10();
+        // Ejercicio11();
+        // Ejercicio12();
+        // Ejercicio13();
+         Ejercicio14();
+        //Ejercicio15();
     }
 
     public static void Ejercicio1()
     {
-        int a, b;
-        int resultado = 0;
+        int A, B;
+        int Resultado = 0;
 
         Console.WriteLine("Ingrese un número: ");
-        a = Convert.ToInt32(Console.ReadLine());
+        A = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Ingrese otro número: ");
-        b = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
 
-        resultado = a + b;
+        Resultado = A + B;
 
-        Console.WriteLine("El resultado de la suma es {0}", resultado);
+        Console.WriteLine("El resultado de la suma es {0}", Resultado);
         Console.ReadKey();
     }
 
     public static void Ejercicio2()
     {
-        int a, b;
-        int resultado = 0;
+        int A, B;
+        int Resultado = 0;
 
         Console.WriteLine("Ingrese un número: ");
-        a = Convert.ToInt32(Console.ReadLine());
+        A = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Ingrese otro número: ");
-        b = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
 
-        resultado = a - b;
+        Resultado = A - B;
 
-        Console.WriteLine("Hay {0} números entre los dos valores", resultado);
+        Console.WriteLine("Hay {0} números entre los dos valores", Resultado);
         Console.ReadKey();
     }
 
     public static void Ejercicio3()
     {
-        int a, b, c;
-        int perimetro = 0;
+        int A, B, C;
+        int Perimetro = 0;
 
         Console.WriteLine("Ingrese lado a: "); 
-        a = Convert.ToInt32(Console.ReadLine());
+        A = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Ingrese lado b: ");
-        b = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Ingrese lado c: "); 
-        c = Convert.ToInt32(Console.ReadLine());
+        C = Convert.ToInt32(Console.ReadLine());
 
-        perimetro = a + b + c;
+        Perimetro = A + B + C;
 
-        Console.WriteLine("El perímetro es {0}", perimetro);
+        Console.WriteLine("El perímetro es {0}", Perimetro);
         Console.ReadKey();
     }
 
     public static void Ejercicio4()
     {
-        int a;
-        int superficie = 0;
+        int A;
+        int Superficie = 0;
 
         Console.WriteLine("Ingrese la medida del lado del cuadrado"); 
-        a = Convert.ToInt32(Console.ReadLine());
+        A = Convert.ToInt32(Console.ReadLine());
 
-        superficie = a * a;
+        Superficie = A * A;
 
-        Console.WriteLine("La superficie es {0}", superficie); 
+        Console.WriteLine("La superficie es {0}", Superficie); 
         Console.ReadKey();
     }
 
@@ -138,12 +145,110 @@ class CGF
 
     }
 
-    public static void Ejercicio9() { }
-    public static void Ejercicio10() { }
-    public static void Ejercicio11() { }
-    public static void Ejercicio12() { }
-    public static void Ejercicio13() { }
-    public static void Ejercicio14() { }
-    public static void Ejercicio15() { }
+    public static void Ejercicio9() 
+    {
+        string Cadena1, Cadena2, Concat;
+
+        Console.WriteLine("Ingresa una frase: "); 
+        Cadena1 = Console.ReadLine();
+        Console.WriteLine("Ingresa otra frase: "); 
+        Cadena2 = Console.ReadLine();
+
+        Concat = Cadena1 + Cadena2;
+
+        Console.WriteLine("{0}", Concat);
+        Console.ReadKey();
+
+    }
+    public static void Ejercicio10() 
+    {
+        string Cadena;
+        string CadenaA, CadenaB; 
+
+        Console.WriteLine("Ingrese una frase: ");
+        Cadena = Console.ReadLine();
+
+        CadenaB = Cadena.Substring(Cadena.Length / 2);
+        CadenaA = Cadena.Substring(0, Cadena.Length/2);
+
+        Console.WriteLine("{0} {1}", CadenaB, CadenaA);
+        Console.ReadKey();
+    }
+    public static void Ejercicio11() 
+    {
+        string Cadena;
+
+        Console.WriteLine("Ingresa una frase: "); 
+        Cadena = Console.ReadLine();
+
+        Console.WriteLine("Tiene {0} caracteres", Cadena.Length);
+        Console.ReadKey();
+    }
+    public static void Ejercicio12() 
+    {
+        DateTime DT = DateTime.Now;
+
+        Console.WriteLine("{0}", DT);
+        Console.ReadKey();
+    }
+    public static void Ejercicio13() 
+    {
+        DateTime DT;
+        int year, month, day;
+
+        Console.WriteLine("Ingresa un año: ");
+        year = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa un mes: "); 
+        month = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa un día: "); 
+        day = Convert.ToInt32(Console.ReadLine());
+
+        DT = new DateTime(year, month, day);
+
+        Console.WriteLine("{0}", DT.ToShortDateString());
+        Console.ReadKey();
+    }
+    public static void Ejercicio14() 
+    {
+        DateTime DT1;
+        DateTime DT2; 
+        int YearA, MonthA, DayA;
+        int YearB, MonthB, DayB;
+        int Diferencia = 0; 
+
+        Console.WriteLine("Ingresa día a: ");
+        DayA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa mes a: ");
+        MonthA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa año a: ");
+        YearA = Convert.ToInt32(Console.ReadLine());
+
+        DT1 = new DateTime(YearA, MonthA, DayA);
+
+        Console.WriteLine("Ingresa día b: ");
+        DayB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa mes b: ");
+        MonthB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa año b: ");
+        YearB = Convert.ToInt32(Console.ReadLine());
+
+        DT2 = new DateTime(YearB, MonthB, DayB);
+
+        Diferencia = DT1 > DT2 ? (DT1 - DT2).Days : (DT2 - DT1).Days;
+
+        Console.WriteLine("La diferencia de días es {0}", Diferencia);
+        Console.ReadKey();
+    }
+    public static void Ejercicio15() 
+    {
+        DateTime Navidad = new DateTime(2024, 12, 25);
+        DateTime Hoy = DateTime.Now;
+        int CuentaRegresiva = 0;
+
+        CuentaRegresiva = (Navidad - Hoy).Days;
+
+        Console.WriteLine("Faltan {0} días para navidad", CuentaRegresiva);
+        Console.ReadKey();
+    }
 
 }
