045585090f3aa01d5916d39b7200c78190cae304
lucas-perata <contacto.perata@gmail.com>
2024-08-30 19:03:33 -0300

    Ejercicios

---
+++ TP3/3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> List = new List<string>();
+
+            List.Add(textBox1.Text);
+
+            textBox1.Clear();
+
+            foreach (string i in List) 
+            {
+                textBox2.Text += i;
+                textBox2.Text += " ";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var n = textBox2.Text;
+            var sum = 0; 
+
+            foreach(char i in n)
+            {
+                sum += Convert.ToInt32(i);
+            }
+
+            label1.Text = sum.ToString();
+        }
+    }
+}
